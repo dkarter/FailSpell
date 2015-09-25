2eff0f42e493cb3a5ec9b265dba23373ff017036
Dorian Karter <dkarter@gmail.com>
2015-09-25 16:29:22 -0500

    Add rspec json parser back to lib

---
+++ lib/failspell/spec_runner.rb
@@ -0,0 +1,13 @@
+module FailSpell
+  class SpecRunner
+    attr_reader :spec_path
+
+    def initialize(spec_path)
+      @spec_path = spec_path
+    end
+
+    def run
+      `rspec #{spec_path}`
+    end
+  end
+end
