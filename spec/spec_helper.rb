35ea1c8db8f2c669e0c688f92cfbb2538f1da652
Dorian Karter <dkarter@gmail.com>
2015-10-30 15:31:07 -0500

    Create a flag to include fakefs for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,9 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'rspec'
+require 'fakefs/safe'
+require 'fakefs/spec_helpers'
 require 'failspell'
+
+RSpec.configure do |config|
+  config.include FakeFS::SpecHelpers, fakefs: true
+end
