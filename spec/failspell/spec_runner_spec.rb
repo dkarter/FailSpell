d3e219b7aa6c7fe04ac862d88d684f4bd6f2e205
Dorian Karter <dkarter@gmail.com>
2015-11-20 13:38:14 -0600

    rename context to match method name

--- spec/failspell/spec_runner_spec.rb
+++ spec/failspell/spec_runner_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe FailSpell::SpecRunner do
-  context '#run_spec' do
+  context '#run' do
     it 'runs spec on system' do
       runner = described_class.new('some_path:123')
       expect(runner).to receive(:system).with('rspec some_path:123') { 'success' }
