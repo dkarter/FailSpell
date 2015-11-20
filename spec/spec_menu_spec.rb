1b817da9a65389fe12818b902afd2972dcd8430f
Dorian Karter <dkarter@gmail.com>
2015-11-20 13:39:42 -0600

    add pending specs for spec menu

---
+++ spec/spec_menu_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe FailSpell::SpecMenu do
+  context '.show' do
+    xit 'lists all specs to stdout with index'
+    xit 'prompts which one you would like to run'
+    xit 'executes given block when one is provided'
+  end
+end
