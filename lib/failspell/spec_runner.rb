dd6e673cb2fd3a44db4ebe5567fe021fadf69f23
Dorian Karter <dkarter@gmail.com>
2015-11-20 13:36:59 -0600

    Remove run_single_and_store_result
    It is not needed anymore since we get a boolean response from system run
    anyway when running single

--- lib/failspell/spec_runner.rb
+++ lib/failspell/spec_runner.rb
@@ -14,19 +14,6 @@ module FailSpell
       system "rspec #{spec_path} -f p -f j -o ./tmp/failspell_last_run.json"
     end
 
-    def run_single_and_return_result
-      tmp_result_file = './tmp/tmp_run'
-      system "rspec #{spec_path} -f p -f j -o #{tmp_result_file}"
-      if (File.exists?(tmp_result_file))
-        result_json = JSON.parse(File.read(tmp_result_file))
-        result = result_json['summary']['failure_count'] == 0
-        FileUtils.rm_f(tmp_result_file)
-        result
-      else
-        false
-      end
-    end
-
     private
 
     attr_reader :spec_path
--- spec/failspell/spec_runner_spec.rb
+++ spec/failspell/spec_runner_spec.rb
@@ -9,40 +9,6 @@ describe FailSpell::SpecRunner do
     end
   end
 
-  context '#run_single_and_store_result' do
-    let(:runner) { described_class.new('./some_path.rb:123') }
-    let(:tmp_run_file) { './tmp/tmp_run' }
-
-    before do
-      command = "rspec ./some_path.rb:123 -f p -f j -o #{tmp_run_file}"
-      allow(runner).to receive(:system).with(command)
-    end
-
-    it 'runs the single spec and returns true when spec passed' do
-      failure_result = '{ "summary": { "failure_count": 0 }}'
-      write_result_file(failure_result)
-      expect(runner.run_single_and_return_result).to be_truthy
-    end
-
-    it 'runs the single spec and returns false when spec failed' do
-      success_result = '{ "summary": { "failure_count": 1 }}'
-      write_result_file(success_result)
-      expect(runner.run_single_and_return_result).to be_falsey
-    end
-
-    it 'does not leave temporary file behind' do
-      success_result = '{ "summary": { "failure_count": 1 }}'
-      write_result_file(success_result)
-      runner.run_single_and_return_result
-      expect(File.exist?(tmp_run_file)).to eq(false)
-    end
-
-    def write_result_file(result_json)
-      FileUtils.mkdir_p './tmp'
-      File.open(tmp_run_file, 'w') { |f| f.write(result_json) }
-    end
-  end
-
   context '#run_and_store_results' do
     it 'runs spec file or directory with json formatter' do
       runner = described_class.new('spec/')
